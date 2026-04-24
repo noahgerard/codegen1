b1e0cc21d88cc3fba1f77b9d206a36548e087d47
Noah Gerard <97645851+noahgerard@users.noreply.github.com>
2026-04-24 01:24:34 -0400

    f

--- CodeGen.cpp
+++ CodeGen.cpp
@@ -130,13 +130,13 @@ public:
     int total_arguments = actuals.size();
     int stack_arg_count = total_arguments > 6 ? total_arguments - 6 : 0;
 
-    for (int i = 0; i < total_arguments % 6; i++) {
-      cout << "\tmov\t " << operand_to_string(actuals[i]) << ", "
-           << registers[i] << endl;
+    for (int i = 0; i < stack_arg_count; i++) {
+      cout << "\tpush\t " << operand_to_string(actuals[i]) << endl;
     }
 
-    for (int i = 6; i < total_arguments; i++) {
-      cout << "\tpush\t " << operand_to_string(actuals[i]) << endl;
+    for (int i = stack_arg_count; i < total_arguments; i++) {
+      cout << "\tmov\t " << operand_to_string(actuals[i]) << ", "
+           << registers[i] << endl;
     }
 
     // # make the call
