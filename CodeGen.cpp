ddda874f328ff1e3271fcdfb8e22491b15f59d83
Noah Gerard <97645851+noahgerard@users.noreply.github.com>
2026-04-24 01:04:51 -0400

    f

--- CodeGen.cpp
+++ CodeGen.cpp
@@ -130,7 +130,7 @@ public:
     int total_arguments = actuals.size();
     int stack_arg_count = total_arguments > 6 ? total_arguments - 6 : 0;
 
-    for (int i = total_arguments - 1; i > total_arguments - 6; i--) {
+    for (int i = total_arguments - 1; i >= total_arguments - 6; i--) {
       cout << "\tmov\t " << operand_to_string(actuals[i]) << ", "
            << registers[total_arguments - i] << endl;
     }
